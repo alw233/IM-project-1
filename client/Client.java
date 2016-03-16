23e08a71c8469652c43b7c6c0b265cc467b6766c
Abby <alw233@georgetown.edu>
2016-03-16 12:47:38 -0400

    updated the server file so multiple clients can talk to the server

--- client/Client.java
+++ client/Client.java
@@ -1,19 +1,18 @@
 package client;
+import chatroom.*;
 
 import java.io.*;
 import java.net.*;
 
 public class Client {
 	public Socket clientSocket;
-	
+	public static String hostName;
+	public static int portNumber;
 	public Client(Socket connection)
 	{
 		clientSocket = connection;
-	}
-	
-	
-	
-	
+	}	
+		
 	
 	
     public static void main(String[] args) throws IOException {
@@ -24,32 +23,81 @@ public class Client {
             System.exit(1);
         }
 
-        String hostName = args[0];
-        int portNumber = Integer.parseInt(args[1]);
-
-        try (
-            Socket echoSocket = new Socket(hostName, portNumber);
-            PrintWriter out =
-                new PrintWriter(echoSocket.getOutputStream(), true);
-            BufferedReader in =
-                new BufferedReader(
-                    new InputStreamReader(echoSocket.getInputStream()));
-            BufferedReader stdIn =
-                new BufferedReader(
-                    new InputStreamReader(System.in))
-        ) {
-            String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                out.println(userInput);
-                System.out.println("echo from server: " + in.readLine());
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + hostName);
-            System.exit(1);
-        } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                hostName);
-            System.exit(1);
-        } 
+        hostName = args[0];
+        portNumber = Integer.parseInt(args[1]);
+        
+        runClient();
+        
+        
+    }
+       
+    public static void runClient() throws IOException{
+    	Thread sendMessage = new Thread (new Runnable(){
+    		public void run(){
+    			try {  	
+	                Socket echoSocket = new Socket(hostName, portNumber);
+	                PrintWriter out =
+	                    new PrintWriter(echoSocket.getOutputStream(), true);
+	                BufferedReader in =
+	                    new BufferedReader(
+	                        new InputStreamReader(echoSocket.getInputStream()));
+	                BufferedReader stdIn = new BufferedReader(    
+	                        new InputStreamReader(System.in));
+	                String userInput;
+	                while ((userInput = stdIn.readLine()) != null) {
+	                    out.println(userInput);
+	                    System.out.println("echo from server: " + in.readLine());
+	                }
+	                String inputLine;
+					while ((inputLine = in.readLine()) != null) {							
+							out.println(inputLine);
+							System.out.println("Client: " + inputLine);							
+					}
+	            
+	            } catch (UnknownHostException e) {
+	                System.err.println("Don't know about host " + hostName);
+	                System.exit(1);
+	            } catch (IOException e) {
+	                System.err.println("Couldn't get I/O for the connection to " +
+	                    hostName);
+	                System.exit(1);
+	            } 
+    		}
+    	
+    	});sendMessage.start();
+    	
     }
 }
+//    	Thread recieveMessage = new Thread(new Runnable(){
+//    		public void run(){
+//    			try {
+//					while (model.clientSockets.size() == 1)
+//					{
+//						System.out.println("here 2");
+//						try {
+//							Thread.sleep(100);
+//						} catch (InterruptedException e)
+//						{
+//							//nothing?
+//						}
+//					}
+//				
+//				       PrintWriter out =
+//				                new PrintWriter(model.clientSockets.get(1).clientSocket.getOutputStream(), true);                   
+//				       BufferedReader in = new BufferedReader(
+//				                new InputStreamReader(model.clientSockets.get(1).clientSocket.getInputStream()));
+//				       String inputLine;
+//				       while ((inputLine = in.readLine()) != null) {
+//				    	   out.println(inputLine);
+//				           System.out.println("Client: " + inputLine);
+//				       }
+//					}
+//				 catch (IOException e)
+//				{
+//					System.out.println(e);
+//				}	
+//    		}
+//    	});
+//    		
+//    	}
+
--- server/Server.java
+++ server/Server.java
@@ -1,15 +1,23 @@
 package server;
 
-import java.io.*;
-import chatroom.*;
-import java.util.Scanner;
 
-public class Server {
+import client.*;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import chatroom.*;
+
+public class Server implements Runnable{
 	//server class
 	//x2
+	Socket sSocket;
+	Server(Socket socket){
+		this.sSocket = socket;
+	}
+	
+	
 	public static void main(String[] args) throws IOException {
 		int port;
-		
 		if (args.length != 1) {
 			Scanner scanner = new Scanner(System.in);
             System.out.println("Enter port number: ");
@@ -21,10 +29,40 @@ public class Server {
 	        port = Integer.parseInt(args[0]);
 		}
 		
-		chatroom.ChatRoom chat = new ChatRoom(port);
-		chatroom.ChatRoomView chatView = new ChatRoomView();
-		chatroom.ChatRoomController controller =
-				new ChatRoomController(chat, chatView);
-
+		ServerSocket serverSocket  = new ServerSocket(port);
+		while (true){			
+			Socket socket = serverSocket.accept();
+			new Thread(new Server(socket)).start();
+		}		
 	}
+	
+	public void run(){
+		try{
+				
+				PrintWriter out =
+					new PrintWriter(this.sSocket.getOutputStream(), true); 
+				
+				BufferedReader in = new BufferedReader(
+					new InputStreamReader(this.sSocket.getInputStream()));
+				BufferedReader stdIn =
+					new BufferedReader(
+						new InputStreamReader(System.in));
+				String inputLine;
+				while ((inputLine = in.readLine()) != null) {							
+						out.println(inputLine);
+						System.out.println("Client: " + inputLine);							
+				}
+				String userInput;
+	            while ((userInput = stdIn.readLine()) != null) {
+	                   out.println(userInput);
+	                   System.out.println("echo from server: " + in.readLine());
+	            }
+
+			}
+			catch (IOException e)
+			{
+				System.out.println(e);
+			}
+	}
+
 }
