fb56ea1e6a89ab97c92abb1e09d7af827c973eaa
Abby <alw233@georgetown.edu>
2016-03-16 21:24:14 -0400

    added chatroom controller and view

--- server/Server.java
+++ server/Server.java
@@ -6,91 +6,34 @@ import java.util.ArrayList;
 
 public class Server {
 	public ServerSocket serverSocket;
-	public ArrayList<Client> clientSockets;
-	Client newClient;
+	public ArrayList<Socket> clientSockets;
+	public ArrayList<String> messageHistoryQueue;
+	Socket newClient;
 	
 	public Server(Integer portNumber) throws IOException
 	{
 		serverSocket = new ServerSocket(portNumber);
-		clientSockets = new ArrayList<Client>();
+		clientSockets = new ArrayList<Socket>();
+		messageHistoryQueue = new ArrayList<String>();
 		
 		runChatRoom();
 	}
 	
-	public void addClient() throws IOException {
-		
-		newClient = new Client(serverSocket.accept());
-		clientSockets.add(newClient);
-	}
-	
-	public void receiveMessage(Client c) throws IOException {
-		BufferedReader in = new BufferedReader(
-	                new InputStreamReader(c.clientSocket.getInputStream()));
-		if (in.ready()) {
-			sendMessage(in.readLine());
-		}
-	}
-	
-	public void sendMessage(String inputLine) throws IOException {
-                 
-		System.out.println("in send message");
 
-		for (int i = 0; i < clientSockets.size(); i++)
-		{
-			new PrintWriter(clientSockets.get(i).clientSocket.getOutputStream(), true).println(inputLine);
-		}
-		System.out.println("Client: " + inputLine);
-
-	}
-	
 	public void runChatRoom() throws IOException {
-		startChatRoom();
-
-						
-		new Thread(new Runnable() {
-			public void run() {
-
-					while (clientSockets.size() == 0)
-					{
-						try {
-							Thread.sleep(100);
-						} catch (InterruptedException e)
-						{
-							//nothing?
-						}
-					}
-					System.out.println("starting thread 2");
-					try {
-						while (true)
-						{
-							for (int i = 0; i < clientSockets.size(); i++)
-							{
-								receiveMessage(clientSockets.get(i));
-							}
-						}
-					} catch (IOException e)
-					{
-						e.printStackTrace();
-					}
-
-			}
-		}).start();
 		
 		try {
 			System.out.println("starting thread 1");
 			while (true)
 			{
-				addClient();
+				Socket socket = serverSocket.accept();
+				clientSockets.add(socket);
 				System.out.println("One client added in.");
+				new SocketThread(socket, this).start();
 			}
 		} catch (IOException e) {
 			System.out.println(e);
 		}
 	}
-	
-	
-	public void startChatRoom() {
-		System.out.println("Initializing chat room....");
-		System.out.println("Waiting for clients....");
-	}
+		
 }
--- server/SocketThread.java
+++ server/SocketThread.java
@@ -6,38 +6,45 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class SocketThread extends Thread {
-	Socket socket;
-		  SocketThread(Server server, Socket socket ) {
-		    this.socket = socket;
-		  }
-		
-		  public void run(){
-				try{
-						
-						PrintWriter out =
-							new PrintWriter(this.socket.getOutputStream(), true); 
-						
-						BufferedReader in = new BufferedReader(
-							new InputStreamReader(this.socket.getInputStream()));
-						BufferedReader stdIn =
-							new BufferedReader(
-								new InputStreamReader(System.in));
-						String inputLine;
-						while ((inputLine = in.readLine()) != null) {							
-								out.println(inputLine);
-								System.out.println("Client: " + inputLine);							
-						}
-						String userInput;
-			            while ((userInput = stdIn.readLine()) != null) {
-			                   out.println(userInput);
-			                   System.out.println("echo from server: " + in.readLine());
-			            }
 
+public class SocketThread extends Thread{
+	Socket socket;
+	Server server;
+	int lastMessageSent = 0;
+
+	  public SocketThread( Socket socket, Server server ) {
+		    this.socket = socket;
+		    this.server = server;
+	  }
+	  
+	  public void run() {
+		  try {
+			  System.out.println("Starting socketthread");
+				while (true)
+				{
+					BufferedReader in = new BufferedReader(
+			                new InputStreamReader(socket.getInputStream()));
+					if (in.ready()) {
+						//sendMessage(in.readLine());
+						server.messageHistoryQueue.add(in.readLine());
 					}
-					catch (IOException e)
+					
+					if (server.messageHistoryQueue.size() > lastMessageSent)
 					{
-						System.out.println(e);
+						synchronized (server.messageHistoryQueue) {
+							while (lastMessageSent < server.messageHistoryQueue.size())
+							{
+								String inputLine = server.messageHistoryQueue.get(lastMessageSent);
+								new PrintWriter(socket.getOutputStream(), true).println(inputLine);
+								lastMessageSent++;
+							}
+						}
 					}
+				}
+			} catch (IOException e)
+			{
+				e.printStackTrace();
 			}
+
+	}
 }
